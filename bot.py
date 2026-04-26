9f4ae5bfd04bb858f9fb3c8eb122fe5106511c07
chvrif001 <111813903+chvrif001@users.noreply.github.com>
2026-04-26 22:45:51 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_together import ChatTogether
 from langchain_core.prompts import PromptTemplate
